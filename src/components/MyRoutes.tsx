1e501f349ed318f54d30402f03fbd9808b2d0992
1t-n00b <mummintrol@gmail.com>
2022-12-02 15:21:08 +0300

    add Navigate to Rout /

--- src/components/MyRoutes.tsx
+++ src/components/MyRoutes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
+
+import {useAppSelector} from '../store/store';
 
 import ProfileContainer from './Profile/ProfileContainer';
 import Login from './Login/Login';
@@ -10,10 +12,14 @@ import NotFoundPage from './404NotFound/NotFoundPage';
 import DialogsContainer from './Dialogs/DialogsContainer';
 import InDevelopment from './InDevelopment/InDevelopment';
 
+
 const MyRoutes: React.FC = () => {
+    const myId = useAppSelector(state => state.auth.myData.id);
+
     return (
         <Routes>
             <Route path="/profile/:userId" element={<RequireAuth children={<ProfileContainer/>}/>}/>
+            <Route path="/" element={<Navigate to={`/profile/` + myId}/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/dialogs" element={<RequireAuth children={<DialogsContainer/>}/>}/>
             <Route path="/users" element={<UsersContainer/>}/>
