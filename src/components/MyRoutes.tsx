c332592f6dc096902286d35c466d0bb19a3ce75d
1t-n00b <mummintrol@gmail.com>
2022-12-02 15:35:25 +0300

    ref Rout /

--- src/components/MyRoutes.tsx
+++ src/components/MyRoutes.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import {Navigate, Route, Routes} from 'react-router-dom';
 
-import {useAppSelector} from '../store/store';
-
 import ProfileContainer from './Profile/ProfileContainer';
 import Login from './Login/Login';
 import UsersContainer from './Users/UsersContainer';
@@ -12,14 +10,11 @@ import NotFoundPage from './404NotFound/NotFoundPage';
 import DialogsContainer from './Dialogs/DialogsContainer';
 import InDevelopment from './InDevelopment/InDevelopment';
 
-
 const MyRoutes: React.FC = () => {
-    const myId = useAppSelector(state => state.auth.myData.id);
-
     return (
         <Routes>
             <Route path="/profile/:userId" element={<RequireAuth children={<ProfileContainer/>}/>}/>
-            <Route path="/" element={<Navigate to={`/profile/` + myId}/>}/>
+            <Route path="/" element={<Navigate to={`/users`}/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/dialogs" element={<RequireAuth children={<DialogsContainer/>}/>}/>
             <Route path="/users" element={<UsersContainer/>}/>
