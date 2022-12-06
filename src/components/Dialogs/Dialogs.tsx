bb642de7cf4f56f1bbc158481cb0f547af77255e
1t-n00b <mummintrol@gmail.com>
2022-12-06 14:55:49 +0300

    refactor scroll + send with ctrl+Enter

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,12 @@
-import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useEffect, useRef, useState} from 'react';
 
 import {TextField} from '@mui/material';
 
 import {Button} from '@material-ui/core';
 
 import {MessageType, StatusType} from '../../store/reducers/messagesReducer';
+
+import noAvatar from '../../assets/img/noAvatar.png';
 
 import style from './Dialogs.module.scss';
 
@@ -34,26 +36,42 @@ const Dialogs: React.FC<DialogsPropsType> = ({dialogs, pushMessage, myId, isRead
     const messages = <div className={style.messages} id="messages" onScroll={scrollHandler}>
         {dialogs.map((m, index) =>
             <div key={index} className={`${m.userId === myId ? `${style.myMessage}` : `${style.message}`}`}>
-                <p><span><img src={m.photo} alt="" style={{width: 25, height: 25, borderRadius: '50%'}}/>
+                <p><span><img src={m.photo ? m.photo : noAvatar} alt="" style={{width: 25, height: 25, borderRadius: '50%'}}/>
                     {m.userName}</span> {m.message}</p>
             </div>)}
         <div ref={messagesAnchorRef}></div>
     </div>;
 
     useEffect(() => {
+        let id: NodeJS.Timeout;
         if (!isAutoScroll) {
-            // messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'});
-            messagesAnchorRef.current?.scrollIntoView(true);
+            id = setTimeout(() => {
+                messagesAnchorRef.current?.scrollIntoView({behavior: 'smooth'});
+            }, 300);
         }
+        return () => {
+            clearInterval(id);
+        };
     }, [dialogs]);
 
-    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setMessage(e.currentTarget.value);
+        if (e) {
+
+        }
+
     };
 
     const onClickHandler = () => {
         pushMessage(message);
         setMessage('');
+    };
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.ctrlKey && e.keyCode === 13) {
+            pushMessage(message);
+            setMessage('');
+        }
     };
 
     return (
@@ -62,6 +80,7 @@ const Dialogs: React.FC<DialogsPropsType> = ({dialogs, pushMessage, myId, isRead
             <TextField label="Message"
                        variant="filled" value={message}
                        onChange={onChangeHandler}
+                       onKeyDown={onKeyPressHandler}
                        multiline={true}
                        autoFocus
             />
